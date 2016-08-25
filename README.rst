1f2777f20a93bf8485142a93214773a8ddc59f68
Steve Kuznetsov <skuznets@redhat.com>
2016-08-25 13:51:39 -0400

    Added files necessary for `pip` packaging
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

---
+++ README.rst
@@ -0,0 +1,4 @@
+origin-ci-tool
+--------------
+
+A CLI tool for building, testing and composing OpenShift repositories.
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='oct',
     version='0.1',
+    url='https://www.github.com/stevekuznetsov/origin-ci-tool',
+    maintainer='Steve Kuznetsov',
+    maintainer_email='skuznets@redhat.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
