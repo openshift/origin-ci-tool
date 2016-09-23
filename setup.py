ec66831c844ccf331fc712ea7d1d6359e2fdb15d
Steve Kuznetsov <skuznets@redhat.com>
2016-09-23 12:39:46 -0400

    Surface test dependencies in `setuptools` script
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- setup.py
+++ setup.py
@@ -2,17 +2,23 @@ from setuptools import setup, find_packages
 
 from oct.cli.version import VERSION
 
+base_requires = [
+    'Click',
+    'ansible'
+]
+
 setup(
     name='oct',
     version=VERSION,
     url='https://www.github.com/stevekuznetsov/origin-ci-tool',
     maintainer='Steve Kuznetsov',
     maintainer_email='skuznets@redhat.com',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    install_requires=[
-        'Click',
-        'ansible',
+    install_requires=base_requires,
+    tests_require=base_requires + [
+        'mock',
+        'coverage'
     ],
     entry_points='''
         [console_scripts]
