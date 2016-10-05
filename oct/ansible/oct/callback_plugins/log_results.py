cdb0f3a34d60ca7ce30e9ad06fa1fd3cdc06e556
Steve Kuznetsov <skuznets@redhat.com>
2016-10-05 08:20:25 -0400

    Added documentation and improved PEP8 compliance in logging callback
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- oct/ansible/oct/callback_plugins/log_results.py
+++ oct/ansible/oct/callback_plugins/log_results.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import (absolute_import, division, print_function)
 
 from datetime import datetime
@@ -37,6 +38,16 @@ class CallbackModule(CallbackBase):
         self.current_playbook_log_path = None
 
     def write_log(self, message, logfile=None, data=None):
+        """
+        Write a message to the current playbook log and
+        optionally dump verbose data to a separate log-
+        file, linking the two with another message in
+        the playbook log.
+
+        :param message: message for the playbook log
+        :param logfile: path to extra log file
+        :param data: data for extra log file
+        """
         with open(self.current_playbook_log_path, 'a') as playbook_log:
             playbook_log.write('{} | {}\n'.format(datetime.now(), message))
             if logfile and data:
@@ -45,6 +56,12 @@ class CallbackModule(CallbackBase):
                     log_file.write(dump(data, default_flow_style=False, explicit_start=True))
 
     def log_task_result(self, status, result):
+        """
+        Log the result of a task.
+
+        :param status: exit status of the task
+        :param result: result data from the task
+        """
         host = result._host.get_name()
         message = 'Task finished with status "{}" on host "{}"'.format(status, host)
         logfile = join(self.log_dir_for_task(result._task), '{}_{}.yml'.format(host, status))
@@ -52,20 +69,61 @@ class CallbackModule(CallbackBase):
         self.write_log(message, logfile, data)
 
     def log_dir_for_task(self, task):
+        """
+        Determine the directory in which logs
+        for the given task should be placed.
+
+        :param task: task to inspect
+        :return: absolute path to the task log directory
+        """
         return join(self.log_dir_for_play(task._parent._play), 'task_{}'.format(task._uuid))
 
     def log_dir_for_play(self, play):
+        """
+        Determine the directory in which logs
+        for the given play should be placed.
+
+        :param play: play to inspect
+        :return: absolute path to the play log directory
+        """
         return join(self.log_dir_for_playbook(play._attributes['name']._data_source), 'play_{}'.format(play._uuid))
 
     def log_dir_for_playbook(self, playbook_source):
+        """
+        Determine the directory in which logs
+        for the given playbook should be placed.
+
+        :param playbook_source: playbook location on disk
+        :return: absolute path to the playbook log directory
+        """
         return join(self.log_root_dir, 'playbook_{}'.format(playbook_source.replace(sep, '_')))
 
     def log_message_for_task(self, task, message_prefix):
+        """
+        Log a message for a task. The message will
+        be prepended to a generic stanza identifying
+        the task.
+
+        :param task: task to log for
+        :param message_prefix: message prefix to write
+        """
         task_file = task._attributes['name']._data_source
         task_line = task._attributes['name']._line_number
         self.write_log('{} task with name "task_{}" from "{}:{}".'.format(message_prefix, task._uuid, task_file, task_line))
 
     def v2_playbook_on_start(self, playbook):
+        """
+        Implementation of the callback endpoint to be
+        fired when execution of a new playbook begins.
+        We are only interested in recording the results
+        for the last run of any particular playbook, so
+        if the log directory already exists for this
+        playbook, we will over-write it. Otherwise, we
+        make sure to set up the directory for future
+        writes from other callback handlers.
+
+        :param playbook: playbook which began execution
+        """
         playbook_log_dir = self.log_dir_for_playbook(playbook._file_name)
         if exists(playbook_log_dir):
             rmtree(playbook_log_dir)
@@ -75,30 +133,70 @@ class CallbackModule(CallbackBase):
         self.write_log('Starting execution for playbook at {}'.format(playbook._file_name))
 
     def v2_playbook_on_import_for_host(self, result, imported_file):
+        """
+        Implementation of the callback endpoint to be
+        fired when an import occurs on a host.
+
+        :param result: result of the import action
+        :param imported_file: which file was imported by the host
+        """
         # TODO: determine what this result looks like and place it in the right log dir
         host = result._host.get_name()
         self.write_log('Imported file at "{}" on host "{}"'.format(imported_file, host))
         self.write_log(dump(result, default_flow_style=False, explicit_start=True))
 
     def v2_playbook_on_not_import_for_host(self, result, missing_file):
+        """
+        Implementation of the callback endpoint to be
+        fired when an import fails on a host.
+
+        :param result: result of the import action
+        :param missing_file: which file was not found on the host
+        """
         # TODO: determine what this result looks like and place it in the right log dir
         host = result._host.get_name()
         self.write_log('Failed to import file at "{}" on host "{}"'.format(missing_file, host))
         self.write_log(dump(result, default_flow_style=False, explicit_start=True))
 
     def v2_playbook_on_include(self, included_file):
+        """
+        Implementation of the callback endpoint to be
+        fired when an include statement is executed.
+
+        :param included_file: which file was imported by the host
+        """
         self.write_log('Included file at "{}"'.format(included_file))
 
     def v2_playbook_on_setup(self):
+        """
+        Implementation of the callback endpoint to be
+        fired when a setup task is executed. We want
+        to handle this case separately from the generic
+        task case as setup "tasks" do not contain much
+        of the metadata we expect to see on all tasks.
+        """
         self.write_log('Running setup for playbook.')
 
     def v2_playbook_on_play_start(self, play):
+        """
+        Implementation of the callback endpoint to be
+        fired when execution of a new play begins.
+
+        :param play: play which began execution
+        """
         makedirs(self.log_dir_for_play(play))
         play_file = play._attributes['name']._data_source
         play_line = play._attributes['name']._line_number
         self.write_log('Starting execution for play with name "play_{}" from "{}:{}".'.format(play._uuid, play_file, play_line))
 
     def v2_playbook_on_task_start(self, task, is_conditional):
+        """
+        Implementation of the callback endpoint to be
+        fired when execution of a new task begins.
+
+        :param task:task which began execution
+        :param is_conditional: whether or not this task is conditional
+        """
         makedirs(self.log_dir_for_task(task))
         if task._attributes['action'] != 'setup':
             # setup "tasks" aren't really tasks and don't
@@ -109,30 +207,84 @@ class CallbackModule(CallbackBase):
             self.log_message_for_task(task, 'Starting')
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
+        """
+        Implementation of the callback endpoint to be
+        fired when a task fails to execute successfully.
+
+        :param result: result of the task execution
+        :param ignore_errors: if we should consider this a failure
+        """
         self.log_task_result('failed', result)
 
     def v2_runner_on_ok(self, result):
+        """
+        Implementation of the callback endpoint to be
+        fired when a task executes successfully.
+
+        :param result: result of the task execution
+        """
         self.log_task_result('ok', result)
 
     def v2_runner_on_skipped(self, result):
+        """
+        Implementation of the callback endpoint to be
+        fired when task execution is skipped.
+
+        :param result: result of the task execution
+        """
         self.log_task_result('skipped', result)
 
     def v2_runner_on_unreachable(self, result):
+        """
+        Implementation of the callback endpoint to be
+        fired when a task fails to reach a target host.
+
+        :param result: result of the task execution
+        """
         self.log_task_result('unreachable', result)
 
     def v2_runner_on_no_hosts(self, task):
+        """
+        Implementation of the callback endpoint to be
+        fired when a task finds no hosts to execute on.
+
+        :param task: task which did not execute
+        """
         self.log_message_for_task(task, 'No hosts found for')
 
     def v2_runner_on_async_ok(self, result):
+        """
+        Implementation of the callback endpoint to be
+        fired when a task asynchronously executes
+        successfully.
+
+        :param result: result of the task execution
+        """
         self.log_task_result('async ok', result)
 
     def v2_runner_on_async_failed(self, result):
+        """
+        Implementation of the callback endpoint to be
+        fired when a task fails to execute asynchronously
+        successfully.
+
+        :param result: result of the task execution
+        """
         self.log_task_result('async failed', result)
 
     def v2_playbook_on_no_hosts_matched(self):
+        """
+        Implementation of the callback endpoint to be
+        fired when a playbook finds no hosts to execute on.
+        """
         self.write_log('No hosts matched.')
 
     def v2_playbook_on_no_hosts_remaining(self):
+        """
+        Implementation of the callback endpoint to be
+        fired when a playbook has no hosts remaining
+        to execute on.
+        """
         self.write_log('No hosts remaining.')
 
         # TODO: unimplemented methods follow - do we need them?
