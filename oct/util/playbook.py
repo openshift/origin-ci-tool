cb4940cb2cc76edfed6836b93203ce3b9b768293
Steve Kuznetsov <skuznets@redhat.com>
2016-11-10 10:49:24 -0500

    Search through all playbook directories for playbooks
    In order to allow for simple expression of playbooks in
    this tool and other Ansible repositories we will vendor
    like the OpenShift-Ansible project and its contrib, the
    playbook search needs to honor more than one directory.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- oct/util/playbook.py
+++ oct/util/playbook.py
@@ -1,15 +1,26 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function
 
-from os.path import abspath, dirname, join
+from click import ClickException
+from os.path import abspath, dirname, exists, join
 
 
 def playbook_path(playbook_name):
     """
-    Get the path to the named playbook.
+    Get the path to the named playbook. To allow for
+    as much brevity as possible in the given playbook
+    name, we will attempt to search under:
+      - oct/playbooks
+      - openshift-ansible/playbooks
 
     :param playbook_name: the name of the playbook
     :return: the path to the playbook
     """
     from ..oct import __file__ as root_path
-    return join(abspath(dirname(root_path)), 'ansible', 'oct', 'playbooks', playbook_name + '.yml')
+
+    for parent_repo in ['oct', 'openshift-ansible']:
+        playbook_path = join(abspath(dirname(root_path)), 'ansible', parent_repo, 'playbooks', playbook_name + '.yml')
+        if exists(playbook_path):
+            return playbook_path
+
+    raise ClickException('No playbook named {} found!'.format(playbook_name))
