aefa25a9fa1a03ac89d08184371f8a6471aa12dc
Steve Kuznetsov <skuznets@redhat.com>
2016-11-22 11:24:46 -0500

    Update the openshift-ansible update script
    The script now correctly updates the submodule to the latest
    HEAD from upstream and creates a commit with the changes.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/update-openshfit-ansible-version.sh
+++ hack/update-openshfit-ansible-version.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-git submodule update oct/ansible/openshift-ansible
-
 pushd oct/ansible/openshift-ansible 2>&1 >/dev/null
-sed -i "s/^OPENSHIFT_ANSIBLE_VERSION.*/OPENSHIFT_ANSIBLE_VERSION = '$( git describe )'/" ./../../cli/version.py
-sed -i "s/^OPENSHIFT_ANSIBLE_CHECKOUT.*/OPENSHIFT_ANSIBLE_CHECKOUT = '$( git log -1 --pretty=format:%H )'/" ./../../cli/version.py
+git fetch origin
+git rebase origin/master
+description="$( git describe )"
+sed -i "s/^OPENSHIFT_ANSIBLE_VERSION.*/OPENSHIFT_ANSIBLE_VERSION = '${description}'/" ./../../cli/version.py
+hash="$( git log -1 --pretty=format:%H )"
+sed -i "s/^OPENSHIFT_ANSIBLE_CHECKOUT.*/OPENSHIFT_ANSIBLE_CHECKOUT = '${hash}'/" ./../../cli/version.py
 popd 2>&1 >/dev/null
+
+git add --all oct/ansible/openshift-ansible/
+git add oct/cli/version.py
+git commit --signoff -S -m "Update openshift-ansible submodule to ${hash}"$'\n\n'"Current HEAD is now at ${description}"
