8e64bf5a6771f16e5f6b07724fa698e19dc4e29d
Steve Kuznetsov <skuznets@redhat.com>
2017-09-27 10:57:56 -0700

    Allow for a specific AWS AMI id to be used in provision
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- oct/cli/provision/remote/all_in_one.py
+++ oct/cli/provision/remote/all_in_one.py
@@ -124,6 +124,13 @@ Examples:
     help='VM instance name.',
 )
 @option(
+    '--ami-id',
+    '-a',
+    'ami_id',
+    metavar='ID',
+    help='AWS AMI identifier.',
+)
+@option(
     '--destroy',
     '-d',
     is_flag=True,
@@ -134,7 +141,7 @@ Examples:
 @discrete_ssh_config_option
 @ansible_output_options
 @pass_context
-def all_in_one_command(context, operating_system, provider, stage, name, discrete_ssh_config):
+def all_in_one_command(context, operating_system, provider, stage, name, ami_id, discrete_ssh_config):
     """
     Provision a virtual host for an All-In-One deployment.
 
@@ -143,11 +150,15 @@ def all_in_one_command(context, operating_system, provider, stage, name, discret
     :param provider: provider to use with Vagrant
     :param stage: image stage to base the VM off of
     :param name: name to give to the VM instance
+    :param ami_id: AWS EC2 AMI identifier
     :param discrete_ssh_config: whether to update ~/.ssh/config or write a new file
     """
     configuration = context.obj
     if provider == Provider.aws:
-        provision_with_aws(configuration, operating_system, stage, name, discrete_ssh_config)
+        provision_with_aws(configuration, operating_system, stage, name, ami_id, discrete_ssh_config)
+    else:
+        if ami_id is not None:
+            raise ClickException("An AWS EC2 AMI identifier cannot be provided when launching in {}".format(provider))
 
 
 def destroy(configuration):
@@ -159,7 +170,7 @@ def destroy(configuration):
     configuration.run_playbook(playbook_relative_path='provision/aws_all_in_one_down', )
 
 
-def provision_with_aws(configuration, operating_system, stage, name, discrete_ssh_config):
+def provision_with_aws(configuration, operating_system, stage, name, ami_id, discrete_ssh_config):
     """
     Provision a VM in the cloud using AWS EC2.
 
@@ -167,6 +178,7 @@ def provision_with_aws(configuration, operating_system, stage, name, discrete_ss
     :param operating_system: operating system used for the VM
     :param stage: image stage the VM was based off of
     :param name: name to give to the VM instance
+    :param ami_id: AWS EC2 AMI identifier
     :param discrete_ssh_config: whether to update ~/.ssh/config or write a new file
     """
     if not configuration.aws_client_configuration.keypair_name:
@@ -176,23 +188,28 @@ def provision_with_aws(configuration, operating_system, stage, name, discrete_ss
             'No private key path found! Configure one using:\n  $ oct configure aws-client private_key_path PATH'
         )
 
+    playbook_variables={
+        'origin_ci_aws_hostname': configuration.next_available_vagrant_name,  # TODO: fix this
+        'origin_ci_aws_ami_os': operating_system,
+        'origin_ci_aws_ami_stage': stage,
+        'origin_ci_aws_instance_name': name,
+        'origin_ci_inventory_dir': configuration.ansible_client_configuration.host_list,
+        'origin_ci_aws_keypair_name': configuration.aws_client_configuration.keypair_name,
+        'origin_ci_aws_private_key_path': configuration.aws_client_configuration.private_key_path,
+        'origin_ci_ssh_config_strategy': 'discrete' if discrete_ssh_config else 'update',
+        'openshift_schedulable': True,
+        'openshift_node_labels': {
+            'region': 'infra',
+            'zone': 'default',
+        },
+    }
+
+    if ami_id is not None:
+        playbook_variables['origin_ci_aws_ami_id']= ami_id,
+
     configuration.run_playbook(
         playbook_relative_path='provision/aws-up',
-        playbook_variables={
-            'origin_ci_aws_hostname': configuration.next_available_vagrant_name,  # TODO: fix this
-            'origin_ci_aws_ami_os': operating_system,
-            'origin_ci_aws_ami_stage': stage,
-            'origin_ci_aws_instance_name': name,
-            'origin_ci_inventory_dir': configuration.ansible_client_configuration.host_list,
-            'origin_ci_aws_keypair_name': configuration.aws_client_configuration.keypair_name,
-            'origin_ci_aws_private_key_path': configuration.aws_client_configuration.private_key_path,
-            'origin_ci_ssh_config_strategy': 'discrete' if discrete_ssh_config else 'update',
-            'openshift_schedulable': True,
-            'openshift_node_labels': {
-                'region': 'infra',
-                'zone': 'default',
-            },
-        },
+        playbook_variables=playbook_variables,
     )
 
     if stage == Stage.bare:
